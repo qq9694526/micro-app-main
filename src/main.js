23c4bcec0e1e32c3567dbd2e86a56d4898853332
赵海鹏 <zhaohp@bjhzwq.com>
2023-04-04 15:24:43 +0800

    feat:注册子应用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,26 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+import { registerMicroApps, start } from "qiankun";
+
 new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+registerMicroApps(
+  [
+    {
+      name: "app-vue", // app name registered
+      entry: "//192.168.26.143:8081/",
+      container: "#container",
+      activeRule: "/#/vue",
+    },
+  ],
+  {
+    beforeLoad: (app) => console.log("before load", app.name),
+    beforeMount: [(app) => console.log("before mount", app.name)],
+  }
+);
+
+start();
